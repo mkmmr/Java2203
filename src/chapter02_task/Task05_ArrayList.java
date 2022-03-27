3062fce3c7f69e2a441fd340cb19fc0d5f12b9ac
mkmmr <k.vanilla.0830@gmail.com>
2022-03-27 22:23:49 +0900

    test

--- src/chapter02_task/Task05_ArrayList.java
+++ src/chapter02_task/Task05_ArrayList.java
@@ -1,7 +1,21 @@
 package chapter02_task;
+import java.util.ArrayList;
 
-public class Task05 {
+public class Task05_ArrayList {
 	public static void main(String[] args) {
-		System.out.println();
+		ArrayList<String> fruits = new ArrayList<String>();
+		fruits.add("apple");
+		fruits.add("grape");
+		fruits.add("orange");
+		
+		System.out.println("for文を用いたリスト要素の取り出し");
+		for (int i = 0; i < fruits.size(); i++) {
+			System.out.println(fruits.get(i));
+		}
+		
+		System.out.println("拡張for文を用いたリスト要素の取り出し");
+		for (String s : fruits) {
+			System.out.println(s);
+		}
 	}
 }
