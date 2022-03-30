509b80f68bd2fedaded6fd0e35469f903a53104d
mkmmr <102466228+mkmmr@users.noreply.github.com>
2022-03-30 19:46:33 +0900

    Delete src/chapter02task directory

--- src/chapter02task/Task02sumAPIver.java
+++
@@ -1,8 +0,0 @@
-package chapter02task;
-import java.util.stream.IntStream;
-
-public class Task02sumAPIver {
-	public static void main(String[] args) {
-		System.out.println(IntStream.rangeClosed(1, 10).sum());
-	}
-}
